c306e1aedaeb3f81a54be201b12aff67b125dac6
Rafi <delwar2021bd@gmail.com>
2023-09-12 07:44:13 +0600

    added mark button

--- src/components/countries/Countries.jsx
+++ src/components/countries/Countries.jsx
@@ -1,29 +1,68 @@
-import { useEffect } from "react";
-import { useState } from "react";
-import Country from './../country/Country';
-import "./Countries.css"
+import { useEffect, useState } from "react";
+import Country from "./../country/Country";
+import "./Countries.css";
+
 const Countries = () => {
-    // to set store the value of countries in api we are using useState() hook.....
-    const [countries, SetCountries] = useState([]) ;
+  // to set store the value of countries in api we are using useState() hook.....
+  const [countries, SetCountries] = useState([]);
 
-    // fetching the api data...
-    useEffect(() =>{
-        fetch('https://restcountries.com/v3.1/all')
-            .then(res => res.json())
-            .then(data => SetCountries(data))
-    } , [])
+  // fetching the api data... if we miss the dependency array it will create an loop because it will set data when ever value changes..
+  useEffect(() => {
+    fetch("https://restcountries.com/v3.1/all")
+      .then((res) => res.json())
+      .then((data) => SetCountries(data)); //when we set the country in useState it will set the initial value to set value.
+  }, []);
 
-    return (
-        // calling the component for every data.....
-        <div>
-            <h3>Countries Count: {countries.length}</h3>
-            <div className="country-container">
-                {
-                    countries.map(country => <Country key={country.cca3} country={country}></Country>)
-                }    
-            </div>
+  // to handle the visited countries and show them to the upper....
+  const [visitedCountries, setVisitedCountries] = useState([]);
+  const handleSetVisited = (country) => {
+    const newVisitedCountries = [...visitedCountries, country];
+    setVisitedCountries(newVisitedCountries);
+  };
+
+  return (
+    // calling the component for every data.....
+    <>
+      <h3 className="text-2xl font-bold">
+        Countries Count: {countries.length}
+      </h3>
+      {/* visited country section */}
+      <div>
+        <h3 className="text-2xl font-bold">
+          Country Visited: {visitedCountries.length}
+        </h3>
+        {/* here we are mapping the visited countries triggered from the btn */}
+        <div className="grid grid-cols-4 gap-3">
+          {visitedCountries.map((visited) => {
+            const { flags, population, independent, cca3, name } = visited;
+            return (
+              <>
+                <div>
+                  <img src={flags.png} className="h-56" />
+                  <h2 className="text-2xl font-bold">Name: {name.common}</h2>
+                  <p>Population: {population}</p>
+                  <p>Independent: {independent ? "Yes" : "no"}</p>
+                  <p>
+                    <small>area: {cca3}</small>
+                  </p>
+                </div>
+              </>
+            );
+          })}
         </div>
-    );
+      </div>
+
+      <div className="country-container">
+        {countries.map((country) => (
+          <Country
+            key={country.cca3}
+            handleSetVisited={handleSetVisited}
+            country={country}
+          ></Country>
+        ))}
+      </div>
+    </>
+  );
 };
 
 export default Countries;
--- src/components/country/Country.jsx
+++ src/components/country/Country.jsx
@@ -1,17 +1,28 @@
 import { useState } from "react";
 import "./Country.css";
-const Country = ({ country }) => {
-  const { name, flags, population, independent } = country;
+const Country = ({ country, handleSetVisited }) => {
+  const { name, flags, population, independent, cca3 } = country;
 
+  // handle set visited with useState to store value and handleVisited func to handle and btn to trigger the func
   const [visited, setVisited] = useState(false);
+  const handleVisited = () =>{
+    setVisited(!visited)
+  } ;
+
+  // in here we have handleSetVisited from countries.jsx 
+
   return (
-    <div className={name.common === "Israel" ? "hidden" : "country font-mono space-y-3"}>
-      <img src={flags.png} alt="" className="h-56" />
+    <div className={`country font-mono space-y-3 ${visited ? 'bg-green-600': 'bg-red-400'}`}>
+      <img src={flags.png} className="h-56" />
       <h2 className="text-2xl font-bold">Name: {name.common}</h2>
       <p>Population: {population}</p>
       <p>Independent: {independent ? "Yes" : "no"}</p>
       <h3>{visited ? "Done" : "going"}</h3>
-      <button onClick={() => setVisited(!visited)} className="p-2 text-xl font-bold border-black">Click</button>
+      <div className="flex gap-3 justify-center">
+        <button onClick={handleVisited} className="p-2 text-xl font-bold border-black">Click</button>
+        <button onClick={() =>handleSetVisited(country)} className="p-2 text-xl font-bold border-black">Mark Flag</button>
+      </div>
+      <p><small>area: {cca3}</small></p>
     </div>
   );
 };
